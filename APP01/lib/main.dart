46fe06d886f576e5ffe5a77691099929fe55e706
Marco Antonio <201914331j@utea.edu.pe>
2023-09-01 06:28:30 -0500

    trabajo

---
+++ APP01/lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor:
+      // Colors.amber ,
+      color(0xfffb5607),
+      body: SafeArea(child: Text(
+        "Hola Mundo!!!",style:(color: colors.wite,)),
+       ),
+      ),
+    ),
+    );
+}
